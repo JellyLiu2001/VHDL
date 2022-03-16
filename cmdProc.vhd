3eb39df1f5916e3eab68d912df4ecd30a390e64b
ew20673 <79239346+ew20673@users.noreply.github.com>
2022-03-16 12:47:42 +0000

    Add files via upload

--- cmdProc.vhd
+++ cmdProc.vhd
@@ -41,7 +41,7 @@ ARCHITECTURE cmdProc_interim OF cmdProc is
  
  -------set signals
   signal curState, nextState: cmd_state;
-  signal counterN: integer=0;
+  signal counterN: integer:=0;
   singal Asc: std_logic_vector(11 downto 0);
   SIGNAL EN, RST: bit;
 
@@ -55,7 +55,7 @@ begin
        case curState is
           When init => 
               if  rxNow ='1' then
-                  nextState <= check
+                  nextState <= Rxdata_ready
               else 
                   nextState   <= IDLE            ----------------waiting the Rx have a byte from the computer
               end if;
@@ -63,39 +63,57 @@ begin
     
     
     
-    
-          when Check =>
-              if countN = 0 then-------count is 0 check the first input from list
+        ----Part 1  input Numwords to data processor.
+        when Check1 =>----- check A/a
+              if countN = 0 then              -------count is 0 check the first input from list
                 if rxData = '01100001' or rxData ='01100101' then------check the first charactor is A/a
-                      nextState <= CORRECT_INPUT;
-      
-                if rxData >= '00110000' and rxData <='00111001' then -----0-9
-                       nextState <= CORRECT_INPUT;
+                      EN= '1'                   -------at this time counter ready to count the number of 0-9 charactors
+                      nextState <= check2;     
                 else 
-                      nextState <= INIT;
+                      
+                      nextState <= init;            --------- not A/a ,back to the init to find the A and a occurs
                 end if;
+              else----counter not 0 ,check if it is a number
+                     nextState <= check2;
+              end if;
+                
+        When  Check2  =>  --------check if it is a number
+              if rxData >= '00110000' and rxData <='00111001' then -----0-9
+                    if counterN <3 then
+                      nextState <= init;
+                    else
+                      nextState <= transmit_receive;
+              else 
+                      RST  = '1'
+                      nextState <= INIT;
+              end if;
     
-  
-  
-  
-  
-  
-  
-  
-  
-        when CORRECT_INPUT => 
-              EN='1';------start to count the number of N
-    
-              RxDone= '1';  
-          
-              if count < 4 then  
-    
-        
-      
-    
-        when transmit =>
+        when transmit_receive =>
               TxData <= RxData;
               TxNow <='1'
+              nextState <= Check_Transmit
+        
+        
+        when check=>
+              TxNow <='0';
+              RxDone <='0';
+              
+              
+             
+        
+        when send =>
+          
+          RST ='1'
+          numWords_bcd(0) <= Q(3 downto 0)
+          numWords_bcd(1) <= Q(7 downto 4)
+          numWords_bcd(2) <= Q(11 downto 8) 
+          nextState <= Tx_trans
+        
+     
+          
+          
+          
+              
     
     
     
