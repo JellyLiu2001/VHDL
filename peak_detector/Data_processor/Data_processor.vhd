0afaefe274348ac43e40b1be5635ea2e52065908
JellyLiu2001 <79320181+JellyLiu2001@users.noreply.github.com>
2022-04-07 13:52:45 +0100

    finnish process in data processor

--- peak_detector/Data_processor/Data_processor.vhd
+++ peak_detector/Data_processor/Data_processor.vhd
@@ -25,14 +25,15 @@ architecture dataConsume_state OF dataConsume IS
   SIGNAL init,first:state_type;--状态
   SIGNAL counter_reset,compare_reset:std_logic;  --清空
   SIGNAL ctrlIn_delayed, ctrlIn_detected,ctrlOut_reg:std_logic;
-  SIGNAL data:std_vector(7 downto 0);--传来的数据
   SIGNAL peak:std_logic;--最大数
+  SIGNAL MAXindex_BCD:BCD_ARRAY_TYPE(2 downto 0);
   SIGNAL COUNTER : integer:=0;--计数器
-  SIGNAL prefix:char_array_type(0 TO 3);--记peak前三位
+  SIGNAL prefix:char_array_type(0 TO 2);--记peak前三位
+  signal suffix:char_array_type(0 to 3);--记peak和三位
   SIGNAL finnal_result:char_array_type(0 TO 7);--记全部
   SIGNAL index, index_peak:integer;--记位置
   SIGNAL DATAREADY : std_logic;
-  SIGNAL Controlforindex, Controlforcomplete : std_logic
+  SIGNAL Controlforindex, Controlforcomplete : std_logic;
 BEGIN 
 
 combi_nextState:process(curState, X)
@@ -89,7 +90,7 @@ END PROCESS;
 
 ----------------------------------------------------
 
-SHIFTER_process:process(data) --存三位
+SHIFTER_prefix:process(data) --存三位
 BEGIN
 IF rising_edge(clk) and ctrlIn_detected='1' THEN
     for i in 0 TO 2 loop
@@ -100,34 +101,44 @@ END PROCESS;
 
 ----------------------------------------------------
 
-COMPARATOR_process:process(clk,counter)
+COMPARATOR_process:process(clk,counter)--比较
 BEGIN
 IF compare_reset='1' or curstate= start THEN--清除
   index_peak<="0";---
 END IF;
 
 ELSIF rising_edge(clk) THEN
-
-  IF peak(7)='1' and data(7)='1' THEN
     IF data > prefix(3) THEN
-      finnal_result(0 TO 3)<=prefix;
       index_peak<=counter;
     END IF;
 
-  ELSIF peak(7)='0' and data(7)='1' THEN
-    peak<=peak;
-  END IF;
-  
-  ELSIF peak(7)='0' and data(7)='0' THEN
-    IF data > finnal_result(3) THEN
-      finnal_result(0 TO 3)<=prefix;
-      index_peak<=counter;
-    END IF;
-
-  ELSE 
-  finnal_result(0 TO 3)<=prefix;
-    index_peak<=counter;
-
-  END IF;
 END IF;
 END PROCESS;
+
+----------------------------------------------------
+
+SHIFTER_suffix:process(counter,index_peak) --存后四位
+BEGIN
+if counter-index_peak=0 then
+  suffix(0)<=data;
+elsif counter-index_peak=1 then
+  suffix(1)<=data;
+elsif counter-index_peak=2 then
+  suffix(2)<=data;
+elsif counter-index_peak=3 then
+  suffix(3)<=data;
+  finnal_result(0 to 2)<=prefix;--全部的7位
+  finnal_result(3 to 6)<=suffix;
+end if;
+END PROCESS;
+
+----------------------------------------------------
+
+maxindex:process(index_peak) --integer输出bcd
+BEGIN
+MAXindex_BCD(2)<=std_logic_vector(to_unsigned(index_peak/100 mod 10,numWords_bcd(0)'length));
+MAXindex_BCD(1)<=std_logic_vector(to_unsigned(index_peak/10 mod 10,numWords_bcd(0)'length));
+MAXindex_BCD(0)<=std_logic_vector(to_unsigned(index_peak/1 mod 10,numWords_bcd(0)'length));
+END PROCESS;
+
+----------------------------------------------------
