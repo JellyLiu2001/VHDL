9597277bae025c84fc5589f145b4fde69468d89b
JellyLiu2001 <79320181+JellyLiu2001@users.noreply.github.com>
2022-04-13 17:26:14 +0100

    fixed bugs

--- peak_detector/Data_processor/Data_processor.vhd
+++ peak_detector/Data_processor/Data_processor.vhd
@@ -7,8 +7,8 @@ use ieee.numeric_std.all;
     port (
       clk: in std_logic;
       reset: in std_logic; -- synchronous reset
-      start: in bit; --check rising edge, when 1, start
-      numWords_bcd: in BCD_ARRAY_TYPE(2 downto 0);--3*4 BCD ANNN
+      start: in bit; --check rising edge, when 1, start--下面的state重名了
+      numWords_bcd: in BCD_ARRAY_TYPE(2 downto 0);--3*4 BCD ANNN--下面重名了
       ctrlIn: in std_logic;--握手
       ctrlOut: out std_logic;--握手
       data: in std_logic_vector(7 downto 0); --从data gen的数据
@@ -22,20 +22,20 @@ use ieee.numeric_std.all;
 
 architecture dataConsume_state OF dataConsume IS 
   TYPE state_type IS(init, first, second, third, fourth, fifth);--define state 
-  SIGNAL init,first,second,third,fourth,fifth:state_type;--Signal of the state
+ -- SIGNAL init,first,second,third,fourth,fifth:state_type;--SIGNAL of the state
   SIGNAL COUNTER_reset,compare_reset,COUNTER_done,SHIFTER_prefix_done,COMPARATOR_done:std_logic;  --erase the COUNTER and comparetor during each cycle.
   SIGNAL ctrlIn_detected, ctrlIn_delayed,ctrlOut_reg:std_logic;
-  SIGNAL data:std_vector(7 downto 0);--data from data generator.
+  SIGNAL data_gen:std_vector(7 downto 0);--data from data generator. 需要改名称~~~~~~~~~
   SIGNAL ctrlIn_delayed, ctrlIn_detected,ctrlOut_reg:std_logic;
   SIGNAL MAXindex_BCD:BCD_ARRAY_TYPE(2 downto 0);--store the maximum number's index.
   SIGNAL COUNTER : integer:=0;--counting the number and store as index. 
   SIGNAL prefix:char_array_type(0 TO 2);--记peak前三位
-  signal suffix:char_array_type(0 to 3);--记peak和三位
+  SIGNAL suffix:char_array_type(0 to 3);--记peak和三位
   SIGNAL finnal_result:char_array_type(0 TO 7);--记全部
   SIGNAL index, index_peak:integer;--记位置
   SIGNAL DATAREADY : std_logic;
   SIGNAL Controlforindex, Controlforcomplete : std_logic;
-  SIGNAL valueofnumwords : integer 0 to 999 --integer value of numWords_BCD
+  SIGNAL valueofnumwords : integer 0 to 999; --integer value of numWords_BCD
 BEGIN 
 valueofnumwords = --transfer BCD to integer
 ctrlOut <= ctrlOut_reg
@@ -43,7 +43,7 @@ ctrlIn_detected = ctrl_In xor ctrlIn_delayed
 combi_nextState:process(curState, start, reset, ctrlIn_detected,DATAREADY, Controlforindex, Controlforcomplete)
   BEGIN 
     CASE curState IS 
-      WHEN init => -- Wait for start signal
+      WHEN init => -- Wait for start SIGNAL
         ctrlOut_reg ='0'
         IF start = '1' THEN
           nextState => first;
@@ -58,7 +58,7 @@ combi_nextState:process(curState, start, reset, ctrlIn_detected,DATAREADY, Contr
         ELSE
           nextState => init;
         END IF
-      WHEN second => --Set dataready signal high and give byte
+      WHEN second => --Set dataready SIGNAL high and give byte
         nextState => third
       WHEN third =>  --stage to see if counter 
         IF COUNTER = valueofnumwords THEN --All bytes has been counted.
@@ -66,13 +66,13 @@ combi_nextState:process(curState, start, reset, ctrlIn_detected,DATAREADY, Contr
         Else 
           nextState => first;--Not all bytes counted, go back to first stage and step to next byte
         END IF
-      WHEN fourth => --All bytes counter, transfer all signal to cmd, then step to seqdone.
+      WHEN fourth => --All bytes counter, transfer all SIGNAL to cmd, then step to seqdone.
         IF Comparator_done ='1' and SHIFTER_prefix_done ='1' THEN --wait for shifter and comparator
           nextState => fifth;
         Else
           nextState => fourth;
         END IF
-      When fifth => --complete, give seqDone high signal
+      When fifth => --complete, give seqDone high SIGNAL
         nextState => init; 
       When others =>
         nextState => init;
@@ -92,7 +92,7 @@ Handshakeprotocol: process(clk)
   END process;
 
 
-Control_signal :process (curstate)
+Control_SIGNAL :process (curstate)
   begin
     DATAREADY ='0'
     COUNTER_reset='0'
@@ -122,7 +122,7 @@ Control_signal :process (curstate)
     END CASE;
   END process;
 
-STAGE_RESET ：process (curState)
+STAGE_RESET:process (curState)
   begin
     IF reset ='1' THEN
       curState <= INIT
@@ -138,7 +138,7 @@ BEGIN
       COUNTER<=0;
   ELSIF rising_edge(clk) THEN
       COUNTER = COUNTER + 1;
-      COUNTER_done='1';--!
+      COUNTER_done='1';--!总结写
   END IF;
 END process;  
 
@@ -147,7 +147,7 @@ END process;
 SHIFTER_prefix:process(data) --存三位
 BEGIN
 IF rising_edge(clk) and ctrlIn_detected='1' THEN
-    for i in 0 TO 2 loop
+    for i in 0 TO 2 loop--3位循环
       prefix(i)<=prefix(i+1);
     END loop;
       prefix(3)<=data;
@@ -163,7 +163,7 @@ IF compare_reset='1' or curstate= start THEN--清零(curstate 应该是init)
 END IF;
 
 ELSIF rising_edge(clk) THEN
-    IF data > prefix(3) THEN
+    IF data > prefix(3) THEN--选择最大的值
       index_peak<=COUNTER;
       COMPARATOR_done='1';--!
     END IF;
@@ -195,5 +195,4 @@ MAXindex_BCD(2)<=std_logic_vector(to_unsigned(index_peak/100 mod 10,numWords_bcd
 MAXindex_BCD(1)<=std_logic_vector(to_unsigned(index_peak/10 mod 10,numWords_bcd(0)'length));
 MAXindex_BCD(0)<=std_logic_vector(to_unsigned(index_peak/1 mod 10,numWords_bcd(0)'length));
 END process;
-
 ----------------------------------------------------
